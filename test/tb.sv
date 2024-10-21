4665f774a578d5b49725b2a684b03ccbd6e88a4d
JixingZhou2 <jzhou1@wpi.edu>
2024-10-20 21:12:04 -0400

    Edited top module name

--- test/tb.sv
+++ test/tb.sv
@@ -24,7 +24,7 @@ module tb ();
   wire [7:0] uio_oe;
 
   // Replace tt_um_example with your module name:
-  tt_alu_top u_tt_alu_top (
+  tt_um_tt_alu_top tt_um_u_tt_alu_top (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
